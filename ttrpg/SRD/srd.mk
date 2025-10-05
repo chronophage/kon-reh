d4feaec8a99feaf48913d878bd908610e0c14b3b
Nick Gasper <nick.gasper@state.mn.us>
2025-10-04 22:10:33 -0500

    Updated GM and Player's guides.

---
+++ ttrpg/SRD/srd.mk
@@ -0,0 +1,7 @@
+# srd.mk (same directory as Makefile)
+NAME = Fate's Edge - jSRD.pdf
+FILE = fates-edge-srd-main.tex
+# Optional if auto-detection fails:
+#GIT_ROOT = 
+# Or point directly to your script:
+SCRIPT = "$GIT_ROOT/tools/compile_latex.sh"
