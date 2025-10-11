9f802c96033f766b5258bc3ee0fffc79345c64a3
Nick Gasper <nick.gasper@state.mn.us>
2025-10-11 10:13:59 -0500

    Updates

--- tools/build_ttrpg.sh
+++ tools/build_ttrpg.sh
@@ -2,29 +2,30 @@
 
 set -e
 
+git_root=$(git rev-parse --show-toplevel)
 original_path=$(pwd)
 
 # Compile the SRD
 echo "Building SRD"
-cd ../ttrpg/srd/
+cd $git_root/ttrpg/srd/
 ../../tools/compile_latex.sh -f fates-edge-srd-main.tex -n "Fate's Edge - SRD.pdf" > /dev/null 2>&1|| echo "SRD did not build"
 cd - > /dev/null 2>&1
 
 # Compile the Player's Guide
 echo "Building Player's Guide"
-cd ../ttrpg/player_guide/
+cd $git_root/ttrpg/player_guide
 ../../tools/compile_latex.sh -f fates_edge_players_guide.tex -n "Fate's Edge - Player's Guide.pdf" > /dev/null 2>&1|| echo "Player's Guide did not build"
 cd - > /dev/null 2>&1
 
 # Compile the GM Guide
 echo "Building GM Guide"
-cd ../ttrpg/gm_guide
+cd $git_root/ttrpg/gm_guide
 ../../tools/compile_latex.sh -f gm_guide.tex -n "Fate's Edge - GM Guide.pdf"> /dev/null 2>&1|| echo "GM Guide did not build"
 cd - > /dev/null 2>&1
 
 # Compile the Resource Guide
 echo "Building Comprehensive Resource Guide"
-cd ../ttrpg/reference
+cd $git_root/ttrpg/reference
 ../../tools/compile_latex.sh -f fates-edge-resource-guide.tex -n "Fate's Edge - Comprehensive Resource Guide.pdf" > /dev/null 2>&1|| echo "Resource Guide did not build"
 
 echo "done"
