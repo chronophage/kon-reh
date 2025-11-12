c3409b034166f86c465258604b4013192a19336f
Nick Gasper <nick.gasper@state.mn.us>
2025-11-12 12:15:45 -0600

    Updates

--- tools/build_ttrpg.sh
+++ tools/build_ttrpg.sh
@@ -67,7 +67,9 @@ cd $git_root/ttrpg/reference/expansions/
 ../../../tools/compile_latex.sh -f  linns-mists-iron.tex -n "Fate's Edge Expansion - Linns: Mists and Iron.pdf" > /dev/null 2>&1|| echo "#13. Did not build"
 ../../../tools/compile_latex.sh -f  sands-of-moon-and-brass.tex -n "Fate's Edge Expansion - Sands of Moon and Brass.pdf" > /dev/null 2>&1|| echo "#14. Did not build"
 ../../../tools/compile_latex.sh -f  shadows-and-steel.tex -n "Fate's Edge Expansion - Shadows and Steel.pdf" > /dev/null 2>&1|| echo "#15. Did not build"
-
+../../../tools/compile_latex.sh -f  aeler-stone-breath-ledger.tex -n "Fate's Edge Expansion - Aeler: Stone, Breath, and Ledger.pdf" > /dev/null 2>&1|| echo "#16. Did not build"
+../../../tools/compile_latex.sh -f  iron-and-blood.tex -n "Fate's Edge Expansion - Ykrul: Iron & Blood.pdf" > /dev/null 2>&1|| echo "#17. Did not build"
+../../../tools/compile_latex.sh -f  threshold-folk.tex -n "Fate's Edge Expansion - Threshold Folk: Small Peoples & Hidden Realms.pdf" > /dev/null 2>&1|| echo "#18. Did not build"
 
 echo "Committing and pushing to git"
 cd $git_root/ttrpg/build/
